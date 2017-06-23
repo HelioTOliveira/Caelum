1aac13ec5f1b4f01c7b2e884a255635c79b04953
helio.oliveira <helio.oliveira@empresa>
2017-06-23 11:27:34 -0300

    Subindo Script do Banco de Dados

---
+++ Caelum/Banco de Dados/ScriptBanco.sql
@@ -0,0 +1,15 @@
+create database fj21;
+
+-- Ultilizar o banco de dados
+ use fj21;
+ 
+ -- Criar tabela contato
+
+create table contatos (
+id BIGINT NOT NULL AUTO_INCREMENT,
+nome VARCHAR(255),
+email VARCHAR(255),
+endereco VARCHAR(255),
+dataNascimento DATE,
+primary key (id) );
+ 
